6284289925601d8a6b4694c6452918e5704dd645
shittu33 <shittuabdulmujeeb@gmail.com>
2025-06-30 15:13:39 +0100

    Add (handled data passing)

--- dojah-module/src/main/java/com/dojah/kyc_sdk_kotlin/ui/main/fragment/datacollection/DocTypeFragment.kt
+++ dojah-module/src/main/java/com/dojah/kyc_sdk_kotlin/ui/main/fragment/datacollection/DocTypeFragment.kt
@@ -66,35 +66,35 @@ class DocTypeFragment : SpinnerFragment(R.layout.fragment_doc_type) {
 
         binding.apply {
 
-            viewModel.extraUserDataFromPref?.govId.let {
-                if (it != null && it.isAnyFilled()) {
-                    val gIds = govViewModel.getDocIDTypes(viewModel)
-                    val selectedType =
-                        gIds?.first { id -> id?.id == it.getFirstIdTypeFilled(viewModel.dojahEnum)?.id }
-
-                    val selected = selectedType?.name ?: ""
-                    if (selected == GovDocType.NIN.sName) {
-                        //for nin display the idName instead
-                        spinnerTextType.setText(selectedType?.idName ?: "")
-                    } else {
-                        spinnerTextType.setText(selected)
-                    }
-                    viewModel.selectDocType(selected)
-
-                    govViewModel.downloadImageAndConvertToBase64(
-                        url = it.getFirstItemFilled()!!,
-                        onImageDownloaded = { base64 ->
-                            govViewModel.
-                        },
-                        onFailed = { error -> })
-
-//                viewModel.sendUserData(
-//                    firstName = it.firstName!!,
-//                    lastName = it.lastName!!,
-//                    dob = it.dob!!
-//                )
-                }
-            }
+//            viewModel.extraUserDataFromPref?.govId.let {
+//                if (it != null && it.isAnyFilled()) {
+//                    val gIds = govViewModel.getDocIDTypes(viewModel)
+//                    val selectedType =
+//                        gIds?.first { id -> id?.id == it.getFirstIdTypeFilled(viewModel.dojahEnum)?.id }
+//
+//                    val selected = selectedType?.name ?: ""
+//                    if (selected == GovDocType.NIN.sName) {
+//                        //for nin display the idName instead
+//                        spinnerTextType.setText(selectedType?.idName ?: "")
+//                    } else {
+//                        spinnerTextType.setText(selected)
+//                    }
+//                    viewModel.selectDocType(selected)
+//
+//                    govViewModel.downloadImageAndConvertToBase64(
+//                        url = it.getFirstItemFilled()!!,
+//                        onImageDownloaded = { base64 ->
+//                            govViewModel.
+//                        },
+//                        onFailed = { error -> })
+//
+////                viewModel.sendUserData(
+////                    firstName = it.firstName!!,
+////                    lastName = it.lastName!!,
+////                    dob = it.dob!!
+////                )
+//                }
+//            }
             requireActivity().onBackPressedDispatcher.addCallback {
                 if (popupWindow?.isShowing == true) {
                     popupWindow?.dismiss()
